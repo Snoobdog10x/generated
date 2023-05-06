bd7891baec5afb64d6e6d298e3cdca8d31742c4e
Nguyen Duy Thanh <duythanh1565@gmail.com>
2023-05-06 23:22:36 +0700

    fix call notify data changed after dispose

--- abstract_bloc.dart
+++ abstract_bloc.dart
@@ -6,8 +6,16 @@ import 'abstract_state.dart';
 
 abstract class AbstractBloc<T extends AbstractState> extends ChangeNotifier {
   late T state;
+  bool _isDispose = false;
+  @override
+  void dispose() {
+    _isDispose = true;
+    super.dispose();
+  }
+
   AppStore appStore = AppInit.appStore;
   void notifyDataChanged() {
+    if (_isDispose) return;
     notifyListeners();
   }
 }
